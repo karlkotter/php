a611a392306eebf01e412d0d2d340cea3fd0d7df
McKotter <93869121+mckotter@users.noreply.github.com>
2023-05-22 23:40:05 +0300

    Add files via upload

---
+++ kt.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+    <title>KT numero 6</title>
+    <style>
+    .btn-kt {
+        width: 120px;
+        height: 38px;
+    }
+    .jumbotron {
+        
+        height: 600px;
+        background-image: url("https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683__340.png");
+        background-size: cover;
+        color: #fff;
+    }
+    h1 {
+        color: #000;
+        padding-top: 75px;
+        font-family: tahoma, arial, helvetica, sans-serif;
+        font-weight: bold;
+      }
+    p {
+        color: #000;
+    }
+    .rounded-circle {
+        width: 60px;
+        height: 60px;    
+    }
+    .container{
+        width: 1200px;
+    }
+    .Tuim-tekst{
+        padding-top: 70px;
+    }
+    .col-pilt{
+        height: 20px;
+    }
+    .col-tekst{
+        padding-top: 70px;
+    }
+    .joonis2 {
+        padding-top: 90px;
+    }
+    .joonis3tekst {
+        text-align: left;
+    }
+    .joonis3 {
+        padding-top: 110px;
+    }
+    .joonis4 {
+        padding-top: 190px;
+    }
+    .joonis4tekst {
+        text-align: right;
+    }
+    .pilt4 {
+        width: 25%;
+    }
+    .tekst5 {
+        padding-top: 100px;
+    }
+    .kakscolli {
+        padding-top: 30px;
+    }
+    .pikkasi {
+        padding-top: 60px;
+    }
+    .col-md-4 {
+        padding-top: 40px;
+    }
+    .pildidnagudega {
+        padding-top: 90px;
+    }
+    .nupuuud {
+        padding-top: 20px;
+    }
+    .kaart1 {
+        padding-top: 30px;
+        margin-bottom: 60px;
+        
+    }
+    .kaart1 h5 {
+        color: blue;
+    }
+    .kaart1 a {
+        margin-bottom: 40px;
+    }
+    .kaart1tekst {
+        padding-top: 30px;
+    } 
+    .karusell {
+        padding-top: 120px;
+    }
+
+ 
+
+    .imgko {
+        border-radius: 50%;
+        width: 20%;
+        margin-top: 20px;
+        align-self: center;
+        
+    }
+    .sininetaust {
+        margin-top: 30px;
+        background-color: #f5f8fc;
+        position: absolute;
+        width: 100%;
+    }
+    .sininetaust iframe {
+        width: 100%;
+        height: 300px;
+        margin-top: -30px;
+    }
+
+
+
+
+   
+    
+
+    </style>
+</head>
+<body>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <a class="btn btn-primary btn-kt" href="#">KT</a>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Kodu <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">Funkarid</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">Hinnakiri</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-dark" href="#">Tiim</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <a class="nav-link text-dark" href="#">Dokumendid</a>
+        <a class="nav-link text-dark" href="#">Kontaktid</a>
+        <a class="nav-link text-dark" href="#">Logi sisse</a>
+        <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Button</button>
+    </form>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="display-4">Toivo Sillaalune<br>Tuduplats.</h1>
+                    <p class="lead">Kirjutan siia taiesti suvalise teksti kuna<br>lorem tundub haigelt igav olevat ja kell on<br>niipalju et ma tegelikult peaksin magama juba.</p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Lae alla</a>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center align-items-center">
+                <div class="rounded-circle">
+                    <button type="button" class="btn btn-light   rounded-circle">
+                        <i class="fa fa-play" style="color: #000000;"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        </div>
+</div>
+<div class="container">
+    <h2 class="text-center">Joonised</h2>
+
+
+    <div class="3pilti">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="https://cdn.europosters.eu/image/750/posters/star-wars-imperial-fleet-blueprint-i20817.jpg" class="card-img-top" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eligendi eius dolores cum, dolorem vitae iusto, sapiente assumenda magnam incidunt.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="https://media.istockphoto.com/id/172639526/photo/blueprints-chaos-of-lines-vi.jpg?s=612x612&w=0&k=20&c=OYy22-3945OZcPt8lQ-oLRn2CU3wY8JvJmuOoK4ZmCo=" class="card-img-top" alt="">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet at officiis natus atque provident? Fugit distinctio quasi itaque aliquid, aut expedita in laboriosam ullam totam possimus voluptatum quos reiciendis tempore. <a href="#">Hihi link</a>.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="https://media.istockphoto.com/id/510230824/vector/blueprint-of-building.jpg?s=612x612&w=0&k=20&c=rthhicQYSvj9a-Ejo7WtfeDMrRytzFqQkUUN80LFJDQ=" class="card-img-top" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque explicabo vero temporibus unde necessitatibus deserunt aperiam optio cum fuga fugiat! Consequuntur qui suscipit dolore facilis accusamus tempore quia numquam nisi. <a href="#">Hihi link 2</a>.</p>
+                </div>
+            </div>
+
+
+    <div class="joonis2">
+        <div class="row align-items-center">
+            <div class="col">
+            <div class="joonis2tekst">
+                <h2 class="text-left">Joonised</h2>
+                    <p>Kena piff ju</p>
+
+                </div>
+            </div>
+            <div class="col text-right">
+                <img src="https://fastly.picsum.photos/id/996/500/300.jpg?hmac=4qozSuMAhkOyi09LhINMv16NVbRlxp29pxVf20udyTo" alt="Pilt">
+            </div>
+            </div>
+        </div>
+
+        <div class="pikkasi">
+            <div class="row">
+                <div class="pilt4 col-md-4">
+                    <img src="https://fastly.picsum.photos/id/507/300/450.jpg?hmac=pCZefiG9YG8DZt-qpDuIRThLQps_r5H82DeFS7pIe08" alt="...">
+                </div>
+                <div class="tekst5 col-md-8">
+                    <h3>Joonised</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit molestias nemo debitis porro cumque perferendis sed officiis fugiat maxime quis quidem possimus iste, nesciunt accusamus ut quam? Sunt, dolorem hic.</p>
+                    <div class="kakscolli row">
+                        <div class="col">
+                            <h5>Open Source</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consequuntur voluptatem vero, totam dolorum</p>
+                        </div>
+                        <div class="col">
+                            <h5>Bootstrap</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo consequuntur voluptatem vero, totam dolorum</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+    <div class="joonis3">
+        <div class="row align-items-center">
+            <div class="col">
+                <img src="https://fastly.picsum.photos/id/295/500/300.jpg?hmac=sDCbqbtR0aRXecAqYPucyWK2Q4u8OdrAfdGb2QY_PWE" alt="Pilt">
+            </div>
+            <div class="col">
+                <div class="joonis3tekst">
+                    <h2 class="text-left">Joonised</h2>
+                    <p>panen siia paar sona, ja veel paar sona ja ikka veel paar sona</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="joonis4">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="joonis4tekst">
+                    <h2 class="text-right">Joonised</h2>
+                    <p>panen siia paar sona, ja veel paar sona ja ikka veel paar sona</p>
+                </div>
+            </div>
+            <div class="col text-right">
+                <img src="https://fastly.picsum.photos/id/607/500/300.jpg?hmac=bgG3fiPmN_nXIjiVDnpbVXR1ODefxggmLDAYVEcX9ys" alt="Pilt">
+            </div>
+            </div>
+        </div>
+
+
+
+
+    <div class="karusell">
+        <div id="karusell" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <div class="card mx-auto" style="width: 500px;">
+                    <img class="imgko card-img-top" src="https://www.w3schools.com/howto/img_avatar2.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                    <p class="card-text">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita eos numquam voluptates incidunt"</p>
+                    <p class="card-text">Juulius Tseesar</p>
+                    </div>
+                </div>
+                </div>
+                <div class="carousel-item">
+                <div class="card mx-auto" style="width: 500px;">
+                    <img class="imgko card-img-top" src="https://www.w3schools.com/howto/img_avatar.png" alt="Card image cap">
+                    <div class="card-body text-center">
+                    <p class="card-text">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita eos numquam voluptates incidunt"</p>
+                    <p class="card-text">Juulius Tseesar</p>
+                    </div>
+                </div>
+                </div>
+  </div>
+  
+  
+  <a class="carousel-control-prev" role="button" href="#karusell" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Eelmine</span>
+  </a>
+  <a class="carousel-control-next" href="#karusell" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Järgmine</span>
+  </a>
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+    <div class="pildidnagudega">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="https://s.hdnux.com/photos/51/23/24/10827008/4/ratio3x2_1200.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Bobby Brown</h5>
+                    <p class="card-text">UI/IX Designer</p>
+                    <div>
+                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-facebook"></i>
+                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-youtube"></i>
+                    </div>
+                    <div class="nupuuud">
+                        <a href="#" class="btn btn-outline-primary">Kontakteeru</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="https://s.hdnux.com/photos/17/56/51/4123230/4/ratio3x2_1200.jpg" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Shakira</h5>
+                    <p class="card-text">UI/IX Designer</p>
+                    <div>
+                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-facebook"></i>
+                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-youtube"></i>
+                    </div>
+                    <div class="nupuuud">
+                        <a href="#" class="btn btn-outline-primary">Kontakteeru</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="https://s.hdnux.com/photos/26/00/02/5767305/26/rawImage.jpg" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Elon Musk</h5>
+                        <p class="card-text">UI/IX Designer</p>
+                        <div>
+                        <i class="fa fa-twitter"></i>
+                        <i class="fa fa-facebook"></i>
+                        <i class="fa fa-instagram"></i>
+                        <i class="fa fa-youtube"></i>
+                        </div>
+                        <div class="nupuuud">
+                            <a href="#" class="btn btn-outline-primary">Kontakteeru</a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+    <div class="sininetaust">
+
+        <div class="container">
+            <div class="kaart1">
+                    <div class="card text-center">
+                        <div class="card-body">
+                            <h5 class="kaart1tekst card-title">CTO pealkiri</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Call to action</a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199420.20828877145!2d24.69131716460782!3d59.43787608209738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469294ddd21f652f%3A0x726ae145e3e346d!2sTallinn%2C%20Estonia!5e0!3m2!1sen!2sus!4v1616063589236!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        
+
+                <div class="container">
+                    <div class="row" style="margin-top: 80px" >
+                    <div class="col-md-6">
+                        <h2>Contact us</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum odio dolores, libero voluptates iusto quasi omnis doloremque est quia unde? Cum sequi necessitatibus at deleniti officia est repellendus enim quidem?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+                        <p style="padding-top: 30px;">Email: <a href="#">karl.ekotter@gmail.com</a></p>
+                        <p>Telefon: <a href="#">5190 4430</a> </p>
+                    </div>
+                    <div class="col-md-6">
+                        <form>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Eesnimi">
+                            </div>
+                                </div>   
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Perenimi">
+                            </div>
+                                </div>   
+                            </div>   
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="Teema">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" rows="5" placeholder="Kuidas me saab teid aidata?"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Saada</button>
+                        </form>
+                    </div>
+                    </div>
+  </div>
+    <footer style="background-color: #333333; width:100%; height: 60px; margin-top: 40px;"></footer>
+</div>
+
+    
+
+        
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
